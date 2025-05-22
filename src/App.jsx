f1a69e22877788dadec8f4931cae32b930d35ab7
NahuelTG <nahueltorrezg@gmail.com>
2025-05-22 10:02:23 -0400

    update: App js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import { Selector } from './assets/components/Selector.jsx'
 import ARScene from './assets/components/ARScene.jsx'
-import FaceFilter from './assets/components/FaceFilter.jsx'
-// import { ComponenteB } from './ComponenteB'
 import './App.css'
 
 export default function App() {
@@ -11,7 +9,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Selector />} />
         <Route path="/Bee" element={<ARScene />} />
-        <Route path="/Filter" element={<FaceFilter />} />
       </Routes>
     </Router>
   )
